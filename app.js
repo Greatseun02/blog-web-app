b4cbbbe55a00d88792665977d13728c0ec905792
Goodnews Adewole <goodnews.adewole@example.com>
2023-02-22 21:15:26 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const { forEach } = require("lodash");
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://goodnewsadewole9:SyWJxxj0Gaf7yGsY@cluster0.wxe614q.mongodb.net/blogDB");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  body: String
+})
+const Blog = mongoose.model("Blog", blogSchema)
+const BlogTrunc = mongoose.model("Blogtrunc", blogSchema)
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+  //when redirected I can get all the items from the database and send
+  BlogTrunc.find({}, function(err, blogs){
+    const truncPostList = blogs
+    res.render("home", {homeStartingContent:homeStartingContent, truncPostList:truncPostList})
+  })
+})
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent:aboutContent})
+ })
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent:contactContent})
+ })
+
+ app.get("/compose", function(req, res){
+  res.render("compose")
+ })
+
+ app.post("/compose" ,function(req, res){
+  const postTitle = req.body.postTitle
+  const postBody = req.body.postBody
+  const truncPostBody = _.truncate(postBody, {
+    "length": 24,
+    "separator": ","
+  })
+  const blogPosttrunc = new BlogTrunc({
+    title:postTitle,
+    body:truncPostBody
+  })
+  blogPosttrunc.save()
+  //when one adds a title and post, I add that into my database. and that's all
+  const blogPost = new Blog({
+    title:postTitle,
+    body:postBody
+  })
+  blogPost.save()
+
+  res.redirect("/")
+ })
+
+
+app.get("/posts/:param", function(req, res){
+  const param = req.params.param
+  paramLowercase = _.lowerCase(param)
+  Blog.find({}, function(err, blogs){
+    blogs.forEach(function(post){
+      postTitleLowercase = _.lowerCase(post.title)
+      if (postTitleLowercase === paramLowercase){
+        res.render("post", {post:post})
+      }
+    })
+  })
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
